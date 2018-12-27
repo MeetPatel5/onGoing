deea35748bc14df730239cf9634ceecb3bb1f524
MeetPatel5 <meet.patel@openxcellinc.com>
2018-12-27 16:17:55 +0530

    Init commit again

--- README.md
+++ README.md
@@ -1,9 +1,2 @@
-# React + Electron = 😍
 
-An example of using create-react-app and Electron.  
-
-## Scripts
-```yarn start``` will start the Electron app and the React app at the same time.  
-```yarn build``` will build the React app and package it along the Electron app.
-
-## Read more
+## Nothing
--- src/App.js
+++ src/App.js
@@ -3,12 +3,21 @@ import { PlayerConsumer, PlayerProvider } from "./components/player";
 import ReactHowler from "react-howler";
 import "./assets/App.css";
 
-import { Button, Icon, Card } from "semantic-ui-react";
+import { Button } from "@material-ui/core";
+import {
+  PlayArrowSharp,
+  PauseSharp,
+  KeyboardArrowRightSharp,
+  KeyboardArrowLeftSharp,
+  FolderOpenSharp
+} from "@material-ui/icons";
+import { Slider } from "@material-ui/lab";
 
 class App extends Component {
   state = {
     isPlaying: true,
-    currentSeek: 0
+    currentSeek: 0,
+    totalLength: 0
   };
 
   togglePlay = () => {
@@ -19,16 +28,24 @@ class App extends Component {
     });
   };
 
+  onLoad = () => {
+    this.setState({
+      totalLength: Math.round(this.music.duration())
+    });
+  };
+
+  componentDidMount() {
+    this.interval = setInterval(() => this.getSeek(), 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
   getSeek = () => {
-    const a = this.music.seek();
-    this.setState(
-      {
-        currentSeek: a
-      },
-      () => {
-        console.log(this.state.currentSeek);
-      }
-    );
+    this.setState({
+      currentSeek: Math.round(this.music.seek()) || 0
+    });
   };
 
   render() {
@@ -47,37 +64,51 @@ class App extends Component {
               songFiles
             } = context;
 
+            const { currentSeek, totalLength } = this.state;
+
             return (
               <Fragment>
-                <Card
-                  fluid
-                  color="red"
-                  header={songFileNames[currentSongFile.songIndex]}
-                />
-                <Button onClick={changeSong}>Play Random Track</Button>
-                <Button onClick={this.getSeek}>get currentSeek</Button>
-                <Icon name="folder outline" size="huge" onClick={loadFiles} />
+                <h1>{songFileNames[currentSongFile.songIndex]}</h1>
+                <Slider value={currentSeek} min={0} max={totalLength} />
+                <h4> {totalLength}</h4>
+                <Button color="secondary" onClick={this.getSeek}>
+                  get currentSeek
+                </Button>
+                <Button color="secondary" onClick={changeSong}>
+                  Play Random Track
+                </Button>
+
+                <FolderOpenSharp onClick={loadFiles} />
+
                 <ReactHowler
                   src={[currentSongFile.song]}
                   playing={isPlaying}
                   ref={ref => (this.music = ref)}
-                  onLoad={this.handleLoadMusic}
+                  onLoad={this.onLoad}
+                  onEnd={() => playNextSong(currentSongFile["songIndex"] + 1)}
                 />
-                <Icon
-                  size="huge"
-                  name="angle left"
+                {/* Icons */}
+                <KeyboardArrowLeftSharp
+                  color="secondary"
                   onClick={() => playPrevSong(currentSongFile["songIndex"] - 1)}
                 />
+
                 {this.state.isPlaying && songFiles.length > 0 ? (
-                  <Icon size="huge" onClick={this.togglePlay} name="pause" />
+                  <PauseSharp
+                    onClick={songFiles.length > 0 ? this.togglePlay : null}
+                    color="primary"
+                  />
                 ) : (
-                  <Icon size="huge" onClick={this.togglePlay} name="play" />
+                  <PlayArrowSharp
+                    onClick={songFiles.length > 0 ? this.togglePlay : null}
+                    color="primary"
+                  />
                 )}
-                <Icon
-                  size="huge"
-                  name="angle right"
+                <KeyboardArrowRightSharp
+                  color="secondary"
                   onClick={() => playNextSong(currentSongFile["songIndex"] + 1)}
                 />
+                {/* Icons */}
               </Fragment>
             );
           }}
