5a76a367c2877f96ff685d606900ece9c277f9f7
MeetPatel5 <meet.patel@openxcellinc.com>
2018-12-31 17:40:00 +0530

    yes , almost done

--- src/App.js
+++ src/App.js
@@ -1,108 +1,146 @@
-import React, { Component, Fragment } from "react";
+import React, { Component } from "react";
 import ReactPlayer from "react-player";
-import { Button, Card, CardContent } from "@material-ui/core";
+import { Card, CardContent } from "@material-ui/core";
 import {
-  PlayArrowSharp,
-  PauseSharp,
-  SkipNextSharp,
-  SkipPreviousSharp,
-  FolderOpenSharp
+   PlayArrowSharp,
+   PauseSharp,
+   SkipNextSharp,
+   SkipPreviousSharp,
+   FolderOpenSharp
 } from "@material-ui/icons";
-import { Slider } from "@material-ui/lab";
+import { PlayerConsumer, PlayerProvider } from "./components/player";
 
 // Styling
 import "./assets/App.scss";
 
 class App extends Component {
-  state = {
-    url: null,
-    pip: false,
-    playing: true,
-    volume: 0.8,
-    muted: false,
-    played: 0,
-    loaded: 0,
-    duration: 0,
-    playbackRate: 1.0,
-    loop: false
-  };
+   state = {
+      playing: true,
+      playedSeconds: 0,
+      played: 0
+   };
 
-  playPause = () => {
-    this.setState({ playing: !this.state.playing });
-  };
+   togglePlayPause = () => {
+      this.setState({ playing: !this.state.playing });
+   };
 
-  onPlay = () => {
-    console.log("onPlay");
-    this.setState({ playing: true });
-  };
+   onSeekMouseDown = e => {
+      this.setState({ seeking: true });
+   };
 
-  onPause = () => {
-    console.log("onPause");
-    this.setState({ playing: false });
-  };
+   onSeekChange = e => {
+      this.setState({ played: parseFloat(e.target.value) });
+   };
 
-  load = url => {
-    this.setState({
-      url: url
-    });
-  };
+   onSeekMouseUp = e => {
+      this.setState({ seeking: false });
+      this.player.seekTo(parseFloat(e.target.value));
+   };
 
-  onProgress = state => {
-    console.log("onProgress", state);
-    // We only want to update time slider if we are not currently seeking
-    if (!this.state.seeking) {
-      this.setState(state);
-    }
-  };
+   onProgress = ({ played, playedSeconds }) => {
+      console.log("onProgress", playedSeconds);
+      const { seeking } = this.state;
+      // We only want to update time slider if we are not currently seeking
+      if (!seeking) {
+         this.setState({
+            played: played,
+            playedSeconds: playedSeconds
+         });
+      }
+   };
 
-  render() {
-    const { url, playing } = this.state;
-    return (
-      <div className="app">
-        <ReactPlayer
-          height={0}
-          width={0}
-          onProgress={this.onProgress}
-          url={url}
-          playing={playing}
-          controls={false}
-        />
-        <Card className="card">
-          <Button
-            variant="contained"
-            onClick={() =>
-              this.load(
-                "https://storage.googleapis.com/media-session/elephants-dream/the-wires.mp3"
-              )
-            }
-          >
-            Load URL
-          </Button>
-          <CardContent className="card-content">
-            <div className="icons">
-              {url && playing ? (
-                <PauseSharp
-                  onClick={this.onPause}
-                  color="secondary"
-                  className="huge"
-                />
-              ) : (
-                <PlayArrowSharp
-                  onClick={this.onPlay}
-                  color="secondary"
-                  className="huge"
-                />
-              )}
+   render() {
+      const { played, playing } = this.state;
+      return (
+         <PlayerProvider>
+            <PlayerConsumer>
+               {context => {
+                  const {
+                     songFiles,
+                     songFileNames,
+                     currentSongFile,
+                     loadFiles,
+                     playNextSong,
+                     playPrevSong
+                  } = context;
+                  return (
+                     <div className="app">
+                        <ReactPlayer
+                           ref={ref => (this.player = ref)}
+                           controls={false}
+                           height={0}
+                           width={0}
+                           url={currentSongFile.song}
+                           playing={playing}
+                           onProgress={this.onProgress}
+                           onEnded={() =>
+                              playNextSong(currentSongFile["songIndex"] + 1)
+                           }
+                        />
 
-              <SkipPreviousSharp color="secondary" className="large" />
-              <SkipNextSharp color="secondary" className="large" />
-              {/* <FolderOpenSharp color="secondary"/> */}
-            </div>
-          </CardContent>
-        </Card>
-      </div>
-    );
-  }
+                        <Card className="card">
+                           <h3>
+                              {songFileNames[currentSongFile["songIndex"]]}
+                           </h3>
+                           <FolderOpenSharp
+                              color="primary"
+                              onClick={loadFiles}
+                           />
+                           <input
+                              type="range"
+                              min={0}
+                              max={1}
+                              step="any"
+                              value={played}
+                              onMouseDown={this.onSeekMouseDown}
+                              onChange={this.onSeekChange}
+                              onMouseUp={this.onSeekMouseUp}
+                           />
+
+                           <CardContent className="card-content">
+                              <div className="icons">
+                                 {playing && currentSongFile["song"] ? (
+                                    <PauseSharp
+                                       onClick={this.togglePlayPause}
+                                       color="secondary"
+                                       className="huge"
+                                    />
+                                 ) : (
+                                    <PlayArrowSharp
+                                       onClick={this.togglePlayPause}
+                                       color="secondary"
+                                       className="huge"
+                                    />
+                                 )}
+
+                                 <SkipPreviousSharp
+                                    onClick={() => {
+                                       playPrevSong(
+                                          currentSongFile["songIndex"] - 1
+                                       );
+                                    }}
+                                    color="secondary"
+                                    className="large"
+                                 />
+                                 <SkipNextSharp
+                                    onClick={() =>
+                                       playNextSong(
+                                          currentSongFile["songIndex"] + 1
+                                       )
+                                    }
+                                    color="secondary"
+                                    className="large"
+                                 />
+                              </div>
+                           </CardContent>
+                        </Card>
+                     </div>
+                  );
+               }}
+            </PlayerConsumer>
+         </PlayerProvider>
+      );
+   }
 }
 
 export default App;
